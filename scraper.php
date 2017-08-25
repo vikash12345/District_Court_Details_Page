6b141df1b4787a33717c04b3b04f5d18ffb18699
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-25 18:14:47 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -38,11 +38,29 @@ for($id = 0; $id <= 2; $id++)
 	if ($link2) {
 
 			//	Paginate all 'View' buttons
-			foreach ($link2->find("//*[@id='w0']/table/tbody") as $element) {
-				echo $element;
-			}
-		
-	}
+			foreach ($link2->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
+				$RowNumb	+=	1;
+				if ($RowNumb != 0) {
+					$CourtName	=	$element->find('./td[2]', 0);
+					$CaseNumbr	=	$element->find('./td[3]', 0);
+					$CaseStats	=	$element->find('./td[4]', 0);
+					$CaseValue	=	$element->find('./td[5]/button', 0);
+					$CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
+					echo $CaseLink	=	str_replace("amp;", "", $CaseLinkR);
+					
+					//	Visit link inside 'View' button
+					$DetailPg	=	file_get_html($CaseLink);
+
+					if ($DetailPg) {
+						//	Assign fields to varilables
+						$InstDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plaintext;
+						$InstDte1st	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plaintext;
+						$Status		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plaintext;
+						$CaseFlDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2)->plaintext;
+						$RestrCode	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3)->plaintext;
+						$USCode		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3)->plaintext;
+						$AdvPSide1	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4)->plaintext;
+						$AdvPSide2	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4)->plaintext;
 				
-}
+}}}}}
 ?>
