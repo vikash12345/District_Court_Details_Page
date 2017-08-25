9ecccc965a632e4abc6d46711ec9d3664d2f1a10
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-25 18:01:11 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -38,14 +38,14 @@ for($id = 1; $id <= 2; $id++)
 	if ($Html) {
 
 			//	Paginate all 'View' buttons
-			foreach ($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
+			foreach ($Html->find("//div[@id='w0']/table[contains(@class,'class='table table-striped table-bordered')]/tbody/tr") as $element) {
 				$RowNumb	+=	1;
 				if ($RowNumb != 0) {
 					$CourtName	=	$element->find('./td[2]', 0);
 					$CaseNumbr	=	$element->find('./td[3]', 0);
 					$CaseStats	=	$element->find('./td[4]', 0);
 					$CaseValue	=	$element->find('./td[5]/button', 0);
-					$CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
+					echo $CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
 					}
 			}
 		
