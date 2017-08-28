66488772b23163db80153734023729fef33ee62a
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-28 17:10:54 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -30,29 +30,29 @@
 
 					if ($DetailPg) {
 						//	Assign fields to varilables
-						$CaseNo		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 0)->plaintext;
-						 $InstDte		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plaintext;
-						 $InstDte1st	=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plaintext;
-						 $Status		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plaintext;
-						echo $CourtName		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 2)->plaintext;
-						 $CaseFlDte		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2)->plaintext;
-						 $RestrCode		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3)->plaintext;
-						 $USCode		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3)->plaintext;
-						 $AdvPSide1		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4)->plaintext;
-						 $AdvPSide2		=	$link2->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4)->plaintext;
+						$CaseNo		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 0)->plaintext;
+						 $InstDte		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plaintext;
+						 $InstDte1st	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plaintext;
+						 $Status		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plaintext;
+						echo $CourtName		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 2)->plaintext;
+						 $CaseFlDte		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2)->plaintext;
+						 $RestrCode		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3)->plaintext;
+						 $USCode		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3)->plaintext;
+						 $AdvPSide1		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4)->plaintext;
+						 $AdvPSide2		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4)->plaintext;
 						 //This is for Parties Side
-						 $Partyside1	=	$link2->find("//*[@id='party_side_1']/tbody/tr/td", 0)->plaintext;
-						 $Partyside2	=	$link2->find("//*[@id='party_side_2']/tbody/tr/td", 0)->plaintext;
+						 $Partyside1	=	$DetailPg->find("//*[@id='party_side_1']/tbody/tr/td", 0)->plaintext;
+						 $Partyside2	=	$DetailPg->find("//*[@id='party_side_2']/tbody/tr/td", 0)->plaintext;
 						 //This is for FIR DETAILS
-						 $FIR			=	$link2->find("//div[@class='container']/table[2]/tbody/tr[1]/td[1]", 0)->plaintext;
-						 $FIRReg		=	$link2->find("//div[@class='container']/table[2]/tbody/tr[1]/td[2]", 0)->plaintext;
-						 $Offence		=	$link2->find("//div[@class='container']/table[2]/tbody/tr[2]/td[1]", 0)->plaintext;
-						 $IncidentDate =	$link2->find("//div[@class='container']/table[2]/tbody/tr[2]/td[2]", 0)->plaintext;
-						 $CaseProperty	=	$link2->find("//div[@class='container']/table[2]/tbody/tr[3]/td[1]", 0)->plaintext;
-						 $NameofIO		=	$link2->find("//div[@class='container']/table[2]/tbody/tr[3]/td[2]", 0)->plaintext;
-						 $ChallanDetail =   $link2->find("//div[@class='container']/table[2]/tbody/tr[4]/td", 0)->plaintext;
-						 $FIRDesc 		= 	$link2->find("//div[@class='container']/table[2]/tbody/tr[5]/td", 0)->plaintext;	
-						 $pagetext 		= $link2->plaintext;
+						 $FIR			=	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[1]/td[1]", 0)->plaintext;
+						 $FIRReg		=	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[1]/td[2]", 0)->plaintext;
+						 $Offence		=	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[2]/td[1]", 0)->plaintext;
+						 $IncidentDate =	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[2]/td[2]", 0)->plaintext;
+						 $CaseProperty	=	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[3]/td[1]", 0)->plaintext;
+						 $NameofIO		=	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[3]/td[2]", 0)->plaintext;
+						 $ChallanDetail =   $DetailPg->find("//div[@class='container']/table[2]/tbody/tr[4]/td", 0)->plaintext;
+						 $FIRDesc 		= 	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[5]/td", 0)->plaintext;	
+						 $pagetext 		= $DetailPg->plaintext;
 				
 						scraperwiki::save_sqlite(array('name'), array('CaseNumbr' => $CaseNumbr, 
 											      'CourtName' => $CourtName, 
