2e9c524cc998681f6cf857d4c469153876eb77c2
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-28 11:20:16 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,42 +1,15 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-
-
-require 'scraperwiki.php';
-require 'scraperwiki/simple_html_dom.php';
+$BaseLink	=	'http://202.61.43.40:8080/';
+	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
+	$RowNumb;
 //
 /** looping over list of ids of doctors **/
 for($id = 0; $id <= 2; $id++)
 	{
-	 $url = ("http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=".$id);
+	$url = ("http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=".$id);
 	$link2 = file_get_html($url);
 	
 	if ($link2) {
-
 			//	Paginate all 'View' buttons
 			foreach ($link2->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
 				$RowNumb	+=	1;
@@ -46,21 +19,23 @@ for($id = 0; $id <= 2; $id++)
 					$CaseStats	=	$element->find('./td[4]', 0);
 					$CaseValue	=	$element->find('./td[5]/button', 0);
 					$CaseLinkR	=	$BaseLink . $CaseValue->attr['value'];
-					echo $CaseLink	=	str_replace("amp;", "", $CaseLinkR);
+					$CaseLink	=	str_replace("amp;", "", $CaseLinkR);
 					
+					if($CaseLink != null)
+					{
 					//	Visit link inside 'View' button
 					$DetailPg	=	file_get_html($CaseLink);
-
 					if ($DetailPg) {
+						
 						//	Assign fields to varilables
-						$InstDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0)->plaintext;
-						$InstDte1st	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1)->plaintext;
-						$Status		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1)->plaintext;
-						$CaseFlDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2)->plaintext;
-						$RestrCode	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3)->plaintext;
-						$USCode		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3)->plaintext;
-						$AdvPSide1	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4)->plaintext;
-						$AdvPSide2	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4)->plaintext;
+						$InstDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 0);
+						$InstDte1st	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 1);
+						echo $Status		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 1);
+						$CaseFlDte	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 2);
+						$RestrCode	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 3);
+						$USCode		=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 3);
+						$AdvPSide1	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[1]", 4);
+						$AdvPSide2	=	$DetailPg->find("//div[@class='container']/table[1]/tbody/tr/td[2]", 4);
 				
-}}}}}
+	}}}}}}
 ?>
