60c7b75e9146821f9f10ebdf9e6f401572248c50
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-28 11:22:05 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,5 +1,7 @@
 <?
-$BaseLink	=	'http://202.61.43.40:8080/';
+	require 'scraperwiki.php';
+	require 'scraperwiki/simple_html_dom.php';
+	$BaseLink	=	'http://202.61.43.40:8080/';
 	$SiteURL	=	'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
 	$RowNumb;
 //
