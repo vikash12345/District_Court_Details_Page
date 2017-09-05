ed8708c88e1153c02a065103ed581c8fdff6c7e3
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-05 18:02:50 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -53,10 +53,14 @@
 						 $ChallanDetail =   $DetailPg->find("//div[@class='container']/table[2]/tbody/tr[4]/td", 0)->plaintext;
 						 $FIRDesc 		= 	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[5]/td", 0)->plaintext;	
 						 $pagetext 		= $DetailPg->plaintext;
-				scraperwiki::save_sqlite(array('name'), array('name' => $info['CaseNo'], 
+				
+						if($info['CaseNo']){
+						scraperwiki::save_sqlite(array('name'), array('name' => $info['CaseNo'], 
 											      'InstDte' => $info['InstDte'], 
 											      'Status' => $info['Status']
 											      ));
+							
+						}
 
 
 				
