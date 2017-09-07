30f2f7dfa35815008bd1d3b9a665e092b6169de4
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-07 17:29:39 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -16,9 +16,7 @@
 
 			//	Paginate all 'View' buttons
 			foreach ($Html->find("//div[@id='w0']/table[contains(@class,'table-striped')]/tbody/tr") as $element) {
-				$RowNumb	+=	1;
-				if ($RowNumb != 0) {
-				
+				if($element) {
 					echo $CourtName	=	$element->find('./td[2]', 0)->plaintext;
 					$CaseNumbr	=	$element->find('./td[3]', 0)->plaintext;
 					$CaseStats	=	$element->find('./td[4]', 0)->plaintext;
