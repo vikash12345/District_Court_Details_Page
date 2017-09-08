6fd4bc3905f2787ff09edf45c08045ebcd1a23ae
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-08 11:40:22 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -52,10 +52,10 @@
 						 $NameofIO		=	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[3]/td[2]", 0)->plaintext;
 						 $ChallanDetail =   $DetailPg->find("//div[@class='container']/table[2]/tbody/tr[4]/td", 0)->plaintext;
 						 $FIRDesc 		= 	$DetailPg->find("//div[@class='container']/table[2]/tbody/tr[5]/td", 0)->plaintext;
-											scraperwiki::save_sqlite(array('name'), array('name' => $CaseNo , 'InstDte' => $InstDte, 'InstDte1st' => $InstDte1st));
 
 						
-					}
+					}											scraperwiki::save_sqlite(array('name'), array('name' => $CaseNo , 'InstDte' => $InstDte, 'InstDte1st' => $InstDte1st));
+
 					
 
 				}
